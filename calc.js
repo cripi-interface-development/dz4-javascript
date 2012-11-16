1b37c2291c6973e76bad1c4e19729fd55ea919fc
mopdobopot <lommovtsev@gmail.com>
2012-11-16 20:00:53 +0600

    Калькулятор
    Все вычисления производит eval();

---
+++ calc.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  var expr = '';
+  $('.but').click(function() {
+	var $cur = $(this).html();
+	if ($cur === 'c')
+		expr = '0'
+	else if ($cur in ['0','1','2','3','4','5','6','7','8','9'])
+	  if (expr === '0')
+		expr = $cur
+	  else
+		expr += $cur
+	else if ($cur === '=')
+	  expr = eval(expr)
+	else
+	  expr = expr + ' ' + $cur + ' ';
+	if (Math.abs(expr) === Infinity)
+		expr = 'Division by zero';
+	$('.display').html(expr);
+	});
+});
