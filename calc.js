57a2e309c8ffae26fd511281065a0249e39f6840
mopdobopot <lommovtsev@gmail.com>
2012-11-16 20:31:29 +0600

    Исправил ошибочку
    Если после вычислений получался "0", а потом начинался ввод цифр, то мы
    видели на дисплее "0132...". Чтобы исправить, достаточно было заменить
    строгое сравнение с учётом типов "===" на нестрогое, с приведением типов
    "=="

--- calc.js
+++ calc.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	if ($cur === 'c')
 		expr = '0'
 	else if ($cur in ['0','1','2','3','4','5','6','7','8','9'])
-	  if (expr === '0')
+	  if (expr == '0')
 		expr = $cur
 	  else
 		expr += $cur
